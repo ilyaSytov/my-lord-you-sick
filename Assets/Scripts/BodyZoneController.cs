48374bfee3ed57bcf9c10c29da9906ec48c41377
IlyaSytov <redfachon@yandex.ru>
2022-08-30 17:59:09 +0300

    Diagnostic realization

--- Assets/Scripts/BodyZoneController.cs
+++ Assets/Scripts/BodyZoneController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class BodyZoneController : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D other) {
@@ -9,6 +10,15 @@ public class BodyZoneController : MonoBehaviour {
             item.color = Color.gray;
         }
         Debug.Log("Yes");
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        Debug.Log("Yes");
+    }
+
+    void Update() {
+        //Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector3.forward);
+        Physics2D.OverlapPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition));
     }
 
 }
