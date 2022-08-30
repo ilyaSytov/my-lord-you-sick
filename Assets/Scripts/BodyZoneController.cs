da50429cda46c666db928d8cd5d92e0fae88b8f6
Ilya Sytov <RedFachon@yandex.ru>
2022-08-30 23:15:11 +0300

    Work contine

--- Assets/Scripts/BodyZoneController.cs
+++ Assets/Scripts/BodyZoneController.cs
@@ -3,22 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class BodyZoneController : MonoBehaviour {
-    private void OnTriggerEnter2D(Collider2D other) {
-        SpriteRenderer[] sprites = GetComponentsInChildren<SpriteRenderer>();
-        foreach (SpriteRenderer item in sprites) {
-            item.color = Color.gray;
+public class BodyZoneController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+    private bool focused;
+    public bool Focused {
+        get { return focused; }
+        set { focused = value; }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        if (focused == false) {
+            SpriteRenderer[] sprites = GetComponentsInChildren<SpriteRenderer>();
+            foreach (SpriteRenderer item in sprites) {
+                item.color = Color.gray;
+            }
         }
-        Debug.Log("Yes");
     }
 
-    private void OnCollisionEnter2D(Collision2D other) {
-        Debug.Log("Yes");
-    }
-
-    void Update() {
-        //Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector3.forward);
-        Physics2D.OverlapPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+    public void OnPointerExit(PointerEventData eventData) {
+        if (focused == false) {
+            SpriteRenderer[] sprites = GetComponentsInChildren<SpriteRenderer>();
+            foreach (SpriteRenderer item in sprites) {
+                item.color = Color.white;
+            }
+        }
     }
 
 }
