b9a4ba5d26cd3eea750b99d08cd1caf98d004c8e
Ilya Sytov <RedFachon@yandex.ru>
2022-08-30 23:15:39 +0300

    Work contine

--- Assets/Scripts/DiagnosticBodySelect.cs
+++ Assets/Scripts/DiagnosticBodySelect.cs
@@ -3,47 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DiagnosticBodySelect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+public class DiagnosticBodySelect : MonoBehaviour, IPointerClickHandler {
     [SerializeField] private GameObject[] bodyZones;
     [SerializeField] private Animator cameraAnimator;
     private GameObject currentZone;
-    private void Start() {
 
-    }
-
-    public void OnPointerEnter(PointerEventData eventData) {
-        Debug.Log("Yes!");
-    }
-
-    public void OnPointerExit(PointerEventData eventData) {
-        
-    }
-
-    private void FixedUpdate() {
-
-    }
-    private void OnMouseEnter() {
+    public void OnPointerClick(PointerEventData eventData) {
         RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector3.forward);
-        currentZone = hit.collider.gameObject;
-        SpriteRenderer[] sprites = currentZone.GetComponentsInChildren<SpriteRenderer>();
-        foreach (SpriteRenderer item in sprites) {
-            item.color = Color.gray;
-        }
-    }
-    private void OnMouseExit() {
-        SpriteRenderer[] sprites = GetComponentsInChildren<SpriteRenderer>();
-        foreach (SpriteRenderer item in sprites) {
-            item.color = Color.white;
-        }
-    }
-
-    private void OnMouseDown() {
-        Debug.Log("Hit");
-        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector3.forward);
-        currentZone = hit.collider.gameObject;
         foreach (GameObject zone in bodyZones) {
+            zone.GetComponent<BodyZoneController>().Focused = true;
+            if (zone == hit.collider.gameObject) {
 
+            }
         }
-
     }
+
 }
