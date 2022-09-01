0508b20b8f6834d25804a3f734d1b3005d69f93f
IlyaSytov <redfachon@yandex.ru>
2022-09-01 14:51:29 +0300

    Select body zone when patient lying on the couch

--- Assets/Scripts/BodyZoneController.cs
+++ Assets/Scripts/BodyZoneController.cs
@@ -4,26 +4,27 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class BodyZoneController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+    [SerializeField] private string zoneName;
     private bool focused;
     public bool Focused {
         get { return focused; }
         set { focused = value; }
     }
+    public string ZoneName => zoneName;
 
     public void OnPointerEnter(PointerEventData eventData) {
-        if (focused == false) {
-            SpriteRenderer[] sprites = GetComponentsInChildren<SpriteRenderer>();
-            foreach (SpriteRenderer item in sprites) {
-                item.color = Color.gray;
-            }
-        }
+        ObjectHighliter(Color.gray);
     }
 
     public void OnPointerExit(PointerEventData eventData) {
+        ObjectHighliter(Color.white);
+    }
+
+    public void ObjectHighliter(Color highlightColor) {
         if (focused == false) {
             SpriteRenderer[] sprites = GetComponentsInChildren<SpriteRenderer>();
             foreach (SpriteRenderer item in sprites) {
-                item.color = Color.white;
+                item.color = highlightColor;
             }
         }
     }
--- Assets/Scripts/DiagnosticBodySelect.cs
+++ Assets/Scripts/DiagnosticBodySelect.cs
@@ -1,21 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
-public class DiagnosticBodySelect : MonoBehaviour, IPointerClickHandler {
-    [SerializeField] private GameObject[] bodyZones;
+public class DiagnosticBodySelect : MonoBehaviour {
+    [SerializeField] private GameObject backButton;
     [SerializeField] private Animator cameraAnimator;
-    private GameObject currentZone;
+    private BodyZoneController currentZone;
+    private DefaultInputActions inputAction;
+    private bool mouseDown = false;
 
-    public void OnPointerClick(PointerEventData eventData) {
-        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector3.forward);
-        foreach (GameObject zone in bodyZones) {
-            zone.GetComponent<BodyZoneController>().Focused = true;
-            if (zone == hit.collider.gameObject) {
+    private void Awake() {
+        inputAction = new DefaultInputActions();
+        backButton.SetActive(false);
+    }
 
+    private void OnEnable() {
+        inputAction.Enable();
+    }
+
+    private void OnDisable() {
+        inputAction.Disable();
+    }
+
+    private void Update() {
+        if (inputAction.UI.Click.ReadValue<float>() == 1 && mouseDown == false) {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector3.forward);
+            if (hit == true && hit.collider.gameObject.TryGetComponent<BodyZoneController>(out currentZone)) {
+                currentZone.ObjectHighliter(Color.white);
+                currentZone.Focused = true;
+                backButton.SetActive(true);
+                Debug.Log("Set");
+                cameraAnimator.SetTrigger(currentZone.ZoneName + "Enter");
+                Debug.Log("Anim");
             }
+            mouseDown = true;
         }
+        if (inputAction.UI.Click.ReadValue<float>() == 0 && mouseDown == true) {
+            mouseDown = false;
+        }
+    }
+
+    public void ExitAnimation() {
+        backButton.SetActive(false);
+        cameraAnimator.SetTrigger(currentZone.ZoneName + "Exit");
+        currentZone.Focused = false;
+
     }
 
 }
